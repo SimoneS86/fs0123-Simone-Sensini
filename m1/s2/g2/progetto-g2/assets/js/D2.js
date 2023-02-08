15fd52f9b045bf3f6a797f8bdf7784eb4697a97f
SimoneS86 <simone.sensini.86@gmail.com>
2023-02-08 11:27:40 +0100

    appunti e fix

--- m1/s2/g2/progetto-g2/assets/js/D2.js
+++ m1/s2/g2/progetto-g2/assets/js/D2.js
@@ -11,13 +11,11 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-Number; //indica una variabile che contiene un valore numerico 
-Boolean; //indica una variabile che può contenere due soli valori: true o false
-String; //indica una variabile che contiene un singolo carattere o una sequenza finita di caratteri
-Object;//indica una variabile che contiene tipi di dati ed altri elementi più complessi 
-null; // segnala l'assenza intenzionale di un "oggetto", è diverso da 0 e nella logica booleana indica una condizione false 
-undefined; //è una variabile non ha un valore assegnato o non definita
-
+Number; //valore numerico 
+Boolean; //ha due soli valori: true o false
+String; //singolo carattere o una sequenza finita di caratteri
+Object;//tipi di dati ed altri elementi più complessi 
+Array; //lista di elementi
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
@@ -29,10 +27,8 @@ console.log(nome);
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-let a = 12;
-let b = 20;
-let somma = a + b;
-console.log(somma);
+
+console.log(12 +20);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
@@ -58,8 +54,9 @@ console.log(NUM);
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-x = (4 - x); 
-console.log(x); 
+let sottrazione = 4 - x; 
+console.log(sottrazione);
+ 
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -67,10 +64,10 @@ console.log(x);
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).*/
 
 
-let name1 = 'john';
-let name2 = 'John';
-
-name1.toLowerCase() == name2.toLowerCase() ? alert('Errore i nomi inseriti sono uguali') : alert('I nomi sono stati inseriti correttamente');
+ let name1 = 'john'
+ let name2 = 'John'
+ console.log('name1 e name2 sono uguali?', name1 === name2)
+ console.log('name1 e name2 in lowercase sono uguali?', name1.toLowerCase() === name2.toLowerCase())
 
 
 
