edb5158f4c1191fec095b588636b00e6f5b1474e
SimoneS86 <simone.sensini.86@gmail.com>
2023-02-12 22:27:21 +0100

    fix completo

--- m1/s2/g5/progetto-g5/assets/js/calc.js
+++ m1/s2/g5/progetto-g5/assets/js/calc.js
@@ -1,19 +1,12 @@
 function digita(n){
     let elemento = n.getAttribute('data-simbolo');
     let display = document.querySelector('#display');
-    if (['/', '*', '-', '+', '.','%','√'].includes(elemento)){
-        if(['/', '*', '-', '+', '.','√','%'].includes(display.value.substring(display.value.length-1, display.value.lenght))){
+    if (['/', '*', '-', '+', '.','%'].includes(elemento)){
+        if(['/', '*', '-', '+', '.','%'].includes(display.value.substring(display.value.length-1, display.value.lenght))){
             display.value = display.value.substring(0, display.value.length-1);
         }
     }
     display.value += elemento;
-}
-
-function totale(){
-    let display = document.querySelector('#display');
-    if (display.value!=''){
-        display.value = eval(display.value);
-    }else {display.value =''}
 }
 
 function reset(){
@@ -22,6 +15,46 @@ function reset(){
 }
 
 function cancel(){
-    let display = document.querySelector('#display')
+    let display = document.querySelector('#display');
     display.value = display.value.substring(0, display.value.length-1);
+}
+
+function radiceQuad(){
+    let display = document.querySelector('#display')
+    if (display.value!=''){
+        display.value = eval(display.value);
+        display.value = Math.sqrt(display.value) 
+    }
+}
+
+function elevaQuad(){
+    let display = document.querySelector('#display')
+    if (display.value!=''){
+        display.value = eval(display.value);
+        display.value = (display.value) * (display.value) 
+    }
+}
+
+function totale(){
+    let display = document.querySelector('#display');
+    if (display.value!=''){
+        display.value = eval(display.value);
+    }
+}
+
+function sleep(s){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + (s*1000));
+}
+
+function creaTruppe(){
+    let scatola = document.querySelector('#container');
+    let ragno = document.createElement("img");
+    let suono = document.querySelector("#crack");
+    suono.play();
+    sleep(1);
+    ragno.src = "assets/img/ragnetto.png";
+    ragno.alt = "ragnetto";
+    scatola.appendChild(ragno);
+    
 }
